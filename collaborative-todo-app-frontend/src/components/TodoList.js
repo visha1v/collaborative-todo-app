4061d67d53573c8e11a634947918365d18c3612f
Vishal Verma <112986978+visha1v@users.noreply.github.com>
2025-01-04 15:51:31 +0530

    v6

--- collaborative-todo-app-frontend/src/components/TodoList.js
+++ collaborative-todo-app-frontend/src/components/TodoList.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
 
@@ -23,13 +25,34 @@ const TodoList = () => {
     setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
 
+  const deleteTodo = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:5001/todos/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        setTodos(todos.filter((todo) => todo._id !== id)); // Remove the todo from the list
+      } else {
+        console.error('Failed to delete todo');
+      }
+    } catch (error) {
+      console.error('Error deleting todo:', error);
+    }
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm onAddTodo={addTodo} />
-      <ul>
+      <TodoForm onAddTodo={addTodo} className="todo-form"/>
+      <ul className="todo-list">
         {todos.map((todo) => (
-          <TodoItem key={todo._id} todo={todo} />
+          <li key={todo._id} className="todo-item">
+            <span>{todo.title}</span>
+            <button onClick={() => deleteTodo(todo._id)} className="delete-btn">
+              <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
+          </li>
         ))}
       </ul>
     </div>
